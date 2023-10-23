a97e10e27926f5ffc6edb8e56a00602b3c8c198e
Marcus Jones <marcusjones8119@gmail.com>
2023-10-22 20:56:19 -0400

    finish U2L3

--- U2L3-DiceRollingCompetition.py
+++ U2L3-DiceRollingCompetition.py
@@ -4,8 +4,8 @@ import time
 rounds = int(input("How many rounds do you want to play? "))
 
 player_points = 0
-point_map = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, -100]
-player_rolls, cpu_rolls = []
+point_map = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+player_rolls, cpu_rolls = [], []
 
 for i in range(0, rounds):
     print(f"\nROUND {i + 1}!")
@@ -29,9 +29,12 @@ for i in range(0, rounds):
     elif round_tied:
         print(f"Player got a tie! No points gained or lost")
     else:
+        player_points -= 100
         print(f"Player loses and loses 100 points!")
 
+    time.sleep(5)
+
 print(f"\n\nTotal points: {player_points}")
-print(f"Average points per round: {round(player_points / rounds)}")
-print(f"Average player roll: {round(sum(player_rolls) / len(player_rolls))}")
-print(f"Average computer roll: {round(sum(cpu_rolls) / len(cpu_rolls))}")
+print(f"Average points per round: {round(player_points / rounds, 1)}")
+print(f"Average player roll: {round(sum(player_rolls) / len(player_rolls), 1)}")
+print(f"Average computer roll: {round(sum(cpu_rolls) / len(cpu_rolls), 1)}")
