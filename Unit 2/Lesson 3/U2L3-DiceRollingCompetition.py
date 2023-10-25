436798361cdb5eed7c544d9aab86b00868853c3d
Marcus <marcusjones8119@gmail.com>
2023-10-25 09:06:37 -0400

    add comments to U2L3

--- Unit 2/Lesson 3/U2L3-DiceRollingCompetition.py
+++ Unit 2/Lesson 3/U2L3-DiceRollingCompetition.py
@@ -1,19 +1,23 @@
 import random
 import time
 
+# initialize the main variables to be used
 rounds = int(input("How many rounds do you want to play? "))
 player_points = 0
 point_map = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
 player_rolls, cpu_rolls = [], []
 
+# main game loop, goes through every round
 for current_round in range(0, rounds):
     print(f"\nROUND {current_round + 1}!")
+    # determine computer's rolls
     cpu_roll = random.randint(1, 20)
     cpu_rolls.append(cpu_roll)
     print(f"The computer rolled a {cpu_roll}")
     round_roll, round_total, round_tries = 0, 0, 0
-    round_tied = False
 
+    # determine the player's rolls this round
+    round_tied = False
     while round_roll < (cpu_roll + 1) and round_tries < 10:
         round_roll = random.randint(1, 20)
         player_rolls.append(round_roll)
@@ -22,6 +26,7 @@ for current_round in range(0, rounds):
         round_tries += 1
         if round_roll == cpu_roll: round_tied = True
     
+    # determine the result of the round
     if round_roll > cpu_roll:
         player_points += (point_map[round_tries - 1])
         print(f"Player wins and gets {point_map[round_tries - 1]} points!")
@@ -33,6 +38,7 @@ for current_round in range(0, rounds):
 
     time.sleep(5)
 
+# Showcase results of all rounds
 print(f"\n\nTotal points: {player_points}")
 print(f"Average points per round: {round(player_points / rounds, 1)}")
 print(f"Average player roll: {round(sum(player_rolls) / len(player_rolls), 1)}")
