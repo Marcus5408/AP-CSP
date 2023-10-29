abb8648ede2c4e0b33f348472bf383e352520014
Marcus Jones <marcusjones8119@gmail.com>
2023-10-28 21:22:44 -0400

    rework watchdog_reload

--- U2/L7/watchdog_reloader.py
+++ U2/L7/watchdog_reloader.py
@@ -3,6 +3,8 @@ import sys
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import tkinter as tk
+from tkinter import ttk
 
 class MyHandler(FileSystemEventHandler):
     def on_any_event(self, event):
@@ -13,16 +15,19 @@ class MyHandler(FileSystemEventHandler):
             print("Reloading...")
             os.execl(sys.executable, sys.executable, *sys.argv)
 
-def start():
+def start(tkinter_root):
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path='.', recursive=True)
     observer.start()
 
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
+    # Start the tkinter mainloop
+    tkinter_root.mainloop()
 
+    # Stop the observer when the mainloop is quit
+    observer.stop()
     observer.join()
+
+    # Restart the tkinter mainloop after a file change is detected
+    tkinter_root.quit()
+    tkinter_root.after(1000, start, tkinter_root)
