951da969f629bb9560d62c776dd162ea6faa63b4
Marcus Jones <marcusjones8119@gmail.com>
2023-11-06 13:08:40 -0500

    more U2L7
    better formatted example steam.json

--- U2/L7/U2L7.py
+++ U2/L7/U2L7.py
@@ -12,47 +12,47 @@ app = QApplication([])
 
 # Create a QTreeWidget object
 wishlist_display = QTreeWidget()
-wishlist_display.setColumnCount(4)
-wishlist_display.setHeaderLabels(["Name", "Price", "Link", "Banner"])
+wishlist_display.setColumnCount(5)
+wishlist_display.setHeaderLabels(["#", "Banner", "Name", "Price", "URL"])
 
 database = []
 
 class GameNotFoundException(Exception):
     """Exception raised when the game is not found in the Steam API."""
 
-    def __init__(self, game_id, message="Game not found"):
-        self.game_id = game_id
+    def __init__(self, steam_id, message="Game not found"):
+        self.steam_id = steam_id
         self.message = message
         super().__init__(self.message)
 
     def __str__(self):
-        return f'{self.game_id} -> {self.message}'
+        return f'{self.steam_id} -> {self.message}'
 
-def get_steam_details(game_id:str):
+def get_steam_details(steam_id:str):
     # Steam API endpoint
-    url = f"http://store.steampowered.com/api/appdetails?appids={game_id}"
-    response = requests.get(url)
-    data = json.loads(response.text)
+    url = f"http://store.steampowered.com/api/appdetails?appids={steam_id}"
+    api_response = requests.get(url, timeout=5)
+    data = json.loads(api_response.text)
 
     # Check if the request was successful
-    if data[str(game_id)]["success"]:
+    if data[str(steam_id)]["success"]:
         # Get the game's name
-        game_name = data[str(game_id)]["data"]["name"]
-        game_price = data[str(game_id)]["data"]["price_overview"]["final_formatted"]
-        game_found = True
+        steam_name = data[str(steam_id)]["data"]["name"]
+        steam_price = data[str(steam_id)]["data"]["price_overview"]["final_formatted"]
+        valid_game = True
     else:
         raise GameNotFoundException(link)
 
     # Get the game's banner image
-    banner_url = f"https://cdn.cloudflare.steamstatic.com/steam/apps/{game_id}/header.jpg"
-    response = requests.get(banner_url, timeout=5)
-    banner_image = Image.open(BytesIO(response.content))
-    banner_image.save(f"{game_name}_banner.png")
-    banner_pixmap = QPixmap(f"{game_name}_banner.png")
+    steam_banner_url = f"https://cdn.cloudflare.steamstatic.com/steam/apps/{steam_id}/header.jpg"
+    api_response = requests.get(steam_banner_url, timeout=5)
+    steam_banner_image = Image.open(BytesIO(api_response.content))
+    steam_banner_image.save(f"{steam_name}_banner.png")
+    steam_banner_pixmap = QPixmap(f"{steam_name}_banner.png")
 
     database.append(data)
 
-    return game_name, game_price, banner_pixmap, game_found
+    return steam_name, steam_price, steam_banner_pixmap, valid_game
 
 
 #    # Add the game to the treeview with the banner image
@@ -77,8 +77,7 @@ game_name, game_price, banner_pixmap, game_found = get_steam_details(str(game_id
 # Check if the request was successful
 if game_found:
     # Get the game's banner image URL
-    print(database)
-    banner_url = database[len(database) - 1]["data"]["header_image"]
+    banner_url = database[len(database) - 1][str(game_id)]["data"]["header_image"]
 
     # Download the banner image
     response = requests.get(banner_url, timeout=5)
@@ -89,21 +88,15 @@ if game_found:
 
     # Create a QPixmap object for the banner image
     banner_pixmap = QPixmap(f"{game_name}_banner.png")
+    banner_pixmap = banner_pixmap.scaled(128, 128, Qt.KeepAspectRatio)
 
     # Add the game to the treeview with the banner image
     item = QTreeWidgetItem(wishlist_display)
-    item.setText(0, game_name)
-    item.setText(1, game_price)
-    item.setText(2, game_link)
-    item.setIcon(3, QIcon(banner_pixmap))
-
-database.append((game_name, game_price, link))
-
-# Add the game to the treeview
-item = QTreeWidgetItem(wishlist_display)
-item.setText(0, str(len(database) - 1))
-item.setText(1, game_price)
-item.setText(2, game_link)
+    item.setText(0, str(len(database) - 1))
+    item.setIcon(1, QIcon(banner_pixmap))
+    item.setText(2, game_name)
+    item.setText(3, game_price)
+    item.setText(4, game_link)
 
 wishlist_display.show()
 
