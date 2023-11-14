2d1f09f76158d082455aece153a52d7ec0fc3635
Marcus Jones <marcusjones8119@gmail.com>
2023-11-14 03:19:39 -0500

    im in everlasting pain

--- U2/L7/rewrite.py
+++ U2/L7/rewrite.py
@@ -2,9 +2,11 @@ import os
 import tkinter as tk
 from tkinter import ttk
 import json
+from typing import final
 import requests
 import re
 import webbrowser
+import random
 
 class Wishlister(tk.Tk):
     def __init__(self):
@@ -103,17 +105,126 @@ class Wishlister(tk.Tk):
         add_item_dialog.add_button.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
         add_item_dialog.cancel_button.grid(row=2, column=2, padx=5, pady=5, sticky="nsew")
 
+        for i in range(2):
+            add_item_dialog.columnconfigure(i, weight=1)
+
     def remove_item_ui(self):
-        pass
+        remove_item_dialog = tk.Toplevel(self)
+        remove_item_dialog.title("Remove Game")
+        remove_item_dialog.geometry("200x300")
+        remove_item_dialog.minsize(200, 300)
+        remove_item_dialog.resizable(False, False)
+
+        remove_item_dialog.title_label = ttk.Label(remove_item_dialog, text="Remove Game")
+        remove_item_dialog.title_label.config(font=("Arial", 12))
+
+        # Create a Listbox and fill it with the items from the Treeview
+        listbox = tk.Listbox(remove_item_dialog)
+        for item in self.wishlist_display.get_children():
+            listbox.insert(tk.END, self.wishlist_display.item(item)["values"][0])
+
+        # Create an "OK" button that deletes the selected item when clicked
+        ok_button = ttk.Button(
+            remove_item_dialog,
+            text="OK",
+            command=lambda: (
+                self.change_database(
+                    action="remove",
+                    game_id=listbox.get(tk.ACTIVE),
+                    data={}
+                ),
+                remove_item_dialog.destroy()
+            )
+        )
+
+        # Create a "Cancel" button that closes the dialog when clicked
+        cancel_button = ttk.Button(remove_item_dialog, text="Cancel", command=remove_item_dialog.destroy)
+        
+        remove_item_dialog.title_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+        listbox.grid(row=1, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+        ok_button.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
+        cancel_button.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
+        
+        for i in range(2):
+            remove_item_dialog.columnconfigure(i, weight=1)
+        remove_item_dialog.rowconfigure(1, weight=1)
 
     def insert_item_ui(self):
-        pass
+        insert_item_dialog = tk.Toplevel(self)
+        insert_item_dialog.title("Insert Game")
+        insert_item_dialog.geometry("200x300")
+        insert_item_dialog.minsize(200, 300)
+        insert_item_dialog.resizable(False, False)
+
+        insert_item_dialog.title_label = ttk.Label(insert_item_dialog, text="Remove Game")
+        insert_item_dialog.title_label.config(font=("Arial", 12))
+        
+        insert_item_dialog.entry_field = ttk.Entry(insert_item_dialog)
+        # Create an "OK" button that inserts the new item when clicked
+        ok_button = ttk.Button(
+            insert_item_dialog,
+            text="OK",
+            command=lambda: (
+                self.change_database(
+                    action="insert",
+                    game_id=self.get_steam_details("id", insert_item_dialog.entry_field.get()),  # Get the entered item
+                    data={
+                        "game_id": self.get_steam_details("id", insert_item_dialog.entry_field.get()),
+                        "name": self.get_steam_details("name", insert_item_dialog.entry_field.get()),
+                        "price": self.get_steam_details("price", insert_item_dialog.entry_field.get()),
+                        "url": insert_item_dialog.entry_field.get()
+                    }
+                ),
+                insert_item_dialog.destroy()
+            )
+        )
+
+        cancel_button = ttk.Button(insert_item_dialog, text="Cancel", command=insert_item_dialog.destroy)
+
+        insert_item_dialog.title_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+        insert_item_dialog.entry_field.grid(row=1, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+        ok_button.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
+        cancel_button.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
 
     def swap_items_ui(self):
-        pass
+        swap_items_dialog = tk.Toplevel(self)
+
+        swap_items_dialog.title_label = tk.Label(swap_items_dialog, text="Swap Items", font=("Arial", 12))
+        swap_items_dialog.item1_label = tk.Label(swap_items_dialog, text="Item 1:")
+        swap_items_dialog.item2_label = tk.Label(swap_items_dialog, text="Item 2:")
+        swap_items_dialog.first_item = ttk.Combobox(swap_items_dialog, values=[self.wishlist_display.item(item)["values"][0] for item in self.wishlist_display.get_children()])
+        swap_items_dialog.second_item = ttk.Combobox(swap_items_dialog, values=[self.wishlist_display.item(item)["values"][0] for item in self.wishlist_display.get_children()])
+
+        # Create an "OK" button that swaps the selected items when clicked
+        ok_button = ttk.Button(
+            swap_items_dialog,
+            text="OK",
+            command=lambda: (
+                self.change_database(
+                    action="swap",
+                    game_id=None,
+                    data={
+                        "name1":swap_items_dialog.first_item.get(),
+                        "name2":swap_items_dialog.second_item.get()
+                    }
+                ),
+                swap_items_dialog.destroy()
+            )
+        )
+
+        cancel_button = ttk.Button(swap_items_dialog, text="Cancel", command=swap_items_dialog.destroy)
+
+        swap_items_dialog.title_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+        swap_items_dialog.item1_label.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+        swap_items_dialog.item2_label.grid(row=1, column=1, padx=5, pady=5, sticky="nsew")
+        swap_items_dialog.first_item.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
+        swap_items_dialog.second_item.grid(row=2, column=1, padx=5, pady=5, sticky="nsew")
+        ok_button.grid(row=3, column=0, padx=5, pady=5, sticky="nsew")
+        cancel_button.grid(row=3, column=1, padx=5, pady=5, sticky="nsew")
+
 
     def shuffle_items_ui(self):
-        pass
+        self.change_database(action="shuffle", game_id=None, data={})
     
     def find_max_ui(self):
         pass
@@ -137,9 +248,14 @@ class Wishlister(tk.Tk):
         elif detail == "name":
             return data[str(game_id)]["data"]["name"]
         elif detail == "price":
-            price = data[str(game_id)]["data"]["price_overview"]["final_formatted"]
-            final_price = float(re.sub(r'[^\d.]+', '', price))
-            return final_price
+            # convert to str because API returns "true" rather than "True"
+            if data[str(game_id)]["data"]["is_free"]:
+                return "$0.00"
+            else:
+                price = data[str(game_id)]["data"]["price_overview"]["final"]
+                final_price = float(f"{price/100:.2f}")
+                # final_price = f"${price/100:.2f}"
+                return final_price
         else:
             raise ValueError("Invalid detail.")
     
@@ -168,6 +284,28 @@ class Wishlister(tk.Tk):
                 "url": data["url"]
             }
             self.database.insert(game_id, data_to_insert)
+        elif action == "swap":
+            # get the names of the items to swap
+            name1 = data.get("name1")
+            name2 = data.get("name2")
+            if name1 is None or name2 is None:
+                raise ValueError("Invalid data parameter. 'name1' and 'name2' keys are required.")
+            # find the indices of the items to swap
+            index1 = None
+            index2 = None
+            for i, item in enumerate(self.database):
+                if item["name"] == name1:
+                    index1 = i
+                elif item["name"] == name2:
+                    index2 = i
+                if index1 is not None and index2 is not None:
+                    break
+            if index1 is None or index2 is None:
+                raise ValueError("Invalid game names. Game names must be in the database.")
+            # swap the items
+            self.database[index1], self.database[index2] = self.database[index2], self.database[index1]
+        elif action == "shuffle":
+            random.shuffle(self.database)
         
         self.update_treeview()
     
@@ -184,10 +322,15 @@ class Wishlister(tk.Tk):
                 text=data["game_id"],
                 values=(
                     data["name"],
-                    data["price"],
+                    f"${data["price"]/100:.2f}",
                     data["url"]
                 )
             )
+        
+        # update the mandatory string and float lists. thanks mr das :E
+        self.mandatory_string_list = [data["name"] for data in self.database]
+        self.mandatory_float_list = [data["price"] for data in self.database]
+        print(f"The strings!! : {self.mandatory_string_list}\n The floats!! : {self.mandatory_float_list}")
     
     def open_url(self, event):
         item = self.wishlist_display.selection()[0]
