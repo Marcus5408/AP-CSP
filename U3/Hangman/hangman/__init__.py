ead38354806609ff294d0dfef020d405c25deda0
Marcus Jones <marcusjones8119@gmail.com>
2023-12-03 23:39:56 -0500

    CHORE(U3-Hangman): change variable names

--- U3/Hangman/hangman/__init__.py
+++ U3/Hangman/hangman/__init__.py
@@ -1,11 +1,6 @@
-from ast import main
-from math import e
-from operator import inv, le
 import random
 import json
-from typing import List, Type, Union
 from colorama import init, Fore, just_fix_windows_console
-from matplotlib import lines
 
 # initialize colorama and various text formatting things
 init()
@@ -26,8 +21,9 @@ def clear_previous_lines(num_lines_to_clear: int) -> None:
 
 def pick_secret_word(category: str = "none", difficulty: str = "none") -> str:
     # get words from json, sanitize inputs
-    with open(f"{__file__.replace("__init__.py", "")}words.json", "r", encoding="utf-8") as f:
-        words = json.load(f)
+    words_file_path = f"{__file__.replace("__init__.py", "")}words.json"
+    with open(words_file_path, "r", encoding="utf-8") as words_file:
+        words = json.load(words_file)
     category = category.lower()
     difficulty = difficulty.lower()
 
@@ -88,14 +84,14 @@ def show_menu(options: dict) -> int:
         print(f"{GREEN}[{number}] {BLUE}{option_name}{' ' * 15}", flush=True)
 
     # get user input
-    selection = ""
-    invalid_inputs = 0
-    while selection not in options or selection == "":
-        selection = input(f"{CYAN}Please enter an option: {RESET}")
-        invalid_inputs += 1 if selection not in options else 0
-    selection = 1 if selection == "" else int(selection)
+    menu_selection = ""
+    invalid_selections = 0
+    while menu_selection not in options or menu_selection == "":
+        menu_selection = input(f"{CYAN}Please enter an option: {RESET}")
+        invalid_selections += 1 if menu_selection not in options else 0
+    menu_selection = 1 if menu_selection == "" else int(menu_selection)
 
-    return selection, invalid_inputs
+    return menu_selection, invalid_selections
 
 
 def show_main_menu() -> int:
@@ -164,7 +160,7 @@ def play_hangman(settings: dict, program_state:str) -> bool:
     return correctly_guessed
 
 
-def configure_game_settings(settings: dict, program_mode:str) -> dict:
+def configure_game_settings(game_settings: dict, program_mode:str) -> dict:
     clear_previous_lines(5 if program_mode == "main_menu" else 0)
     settings_menu = {
         "1": "Category",
@@ -176,9 +172,9 @@ def configure_game_settings(settings: dict, program_mode:str) -> dict:
     }
     while True:
         print(f"{CLEARLINE}{MAGENTA}Game Options")
-        selection, lines_to_replace = show_menu(settings_menu)
-        clear_previous_lines(8 + lines_to_replace)
-        if selection == 1:
+        settings_selection, lines_to_clear = show_menu(settings_menu)
+        clear_previous_lines(8 + lines_to_clear)
+        if settings_selection == 1:
             print(f"{MAGENTA}Game Options - Category")
             category_options = {}
 
@@ -192,16 +188,16 @@ def configure_game_settings(settings: dict, program_mode:str) -> dict:
             category_options[str(len(words) + 2)] = "Go back to Game Options"  # add go back option
 
             # show menu, update settings if updated
-            selection, lines_to_replace = show_menu(category_options)
-            if selection <= (len(words) + 1):
-                settings["category"] = category_options[str(selection)]
-                print(f"{GREEN}Category set to {BLUE}{settings['category']}{GREEN}.")
+            settings_selection, lines_to_clear = show_menu(category_options)
+            if settings_selection <= (len(words) + 1):
+                game_settings["category"] = category_options[str(settings_selection)]
+                print(f"{GREEN}Category set to {BLUE}{game_settings['category']}{GREEN}.")
             else:
-                print(f"{GREEN}Category not changed ({BLUE}{settings['category']}{GREEN}).")
+                print(f"{GREEN}Category not changed ({BLUE}{game_settings['category']}{GREEN}).")
             
             input(f"{YELLOW}Press enter to go back to the settings menu.")
-            clear_previous_lines(len(category_options) + 4 + lines_to_replace)
-        elif selection == 2:
+            clear_previous_lines(len(category_options) + 4 + lines_to_clear)
+        elif settings_selection == 2:
             print(f"{MAGENTA}Game Options - Difficulty")
             difficulty_options = {
                 "1": "Easy",
@@ -210,40 +206,40 @@ def configure_game_settings(settings: dict, program_mode:str) -> dict:
                 "4": "Random",
                 "5": "Go Back to Game Options"
             }
-            selection, lines_to_replace = show_menu(difficulty_options)
-            if selection <= 4:
-                settings["difficulty"] = difficulty_options[str(selection)]
-                print(f"{GREEN}Difficulty set to {BLUE}{settings['difficulty']}{GREEN}.")
+            settings_selection, lines_to_clear = show_menu(difficulty_options)
+            if settings_selection <= 4:
+                game_settings["difficulty"] = difficulty_options[str(settings_selection)]
+                print(f"{GREEN}Difficulty set to {BLUE}{game_settings['difficulty']}{GREEN}.")
             else:
-                print(f"{GREEN}Difficulty not changed ({BLUE}{settings['difficulty']}{GREEN}).")
+                print(f"{GREEN}Difficulty not changed ({BLUE}{game_settings['difficulty']}{GREEN}).")
 
             input(f"{YELLOW}Press enter to go back to the settings menu.")
-            clear_previous_lines(len(difficulty_options) + 4 + lines_to_replace)
-        elif selection == 3:
+            clear_previous_lines(len(difficulty_options) + 4 + lines_to_clear)
+        elif settings_selection == 3:
             print(f"{MAGENTA}Game Options - Lives")
             lives_options = {
                 "1": "Enter a number of lives",
                 "2": "Reset to default (6)",
                 "3": "Go back to Game Options"
             }
-            selection, lines_to_replace = show_menu(lives_options)
+            selection, lines_to_clear = show_menu(lives_options)
             if selection == 1:
                 lives_input = ""
                 while not lives_input.isdigit():
                     lives_input = input(f"{CYAN}Please enter a number of lives: {RESET}")
-                    lines_to_replace += 1 if not lives_input.isdigit() else 0
+                    lines_to_clear += 1 if not lives_input.isdigit() else 0
 
-                settings["lives"] = lives_input
-                print(f"{GREEN}Lives set to {BLUE}{settings['lives']}{GREEN}.")
+                game_settings["lives"] = lives_input
+                print(f"{GREEN}Lives set to {BLUE}{game_settings['lives']}{GREEN}.")
             elif selection == 2:
-                settings["lives"] = 6
+                game_settings["lives"] = 6
                 print(f"{GREEN}Lives reset to {BLUE}6{GREEN}.")
             else:
-                print(f"{GREEN}Lives not changed ({BLUE}{settings['lives']}{GREEN}).")
+                print(f"{GREEN}Lives not changed ({BLUE}{game_settings['lives']}{GREEN}).")
 
             input(f"{YELLOW}Press enter to go back to the settings menu.")
-            clear_previous_lines(len(lives_options) + 5 + lines_to_replace)
-        elif selection == 4:
+            clear_previous_lines(len(lives_options) + 5 + lines_to_clear)
+        elif settings_selection == 4:
             print(f"{MAGENTA}Game Options - Reset Settings")
             reset_options = {
                 "1": "Reset all settings",
@@ -252,40 +248,40 @@ def configure_game_settings(settings: dict, program_mode:str) -> dict:
                 "4": "Reset lives",
                 "5": "Go back to Game Options"
             }
-            selection, lines_to_replace = show_menu(reset_options)
+            selection, lines_to_clear = show_menu(reset_options)
             if selection == 1:
-                settings["category"] = "random"
-                settings["difficulty"] = "random"
-                settings["lives"] = 6
+                game_settings["category"] = "random"
+                game_settings["difficulty"] = "random"
+                game_settings["lives"] = 6
                 print(f"{GREEN}All settings reset to default:")
                 print(f"Category: {BLUE}Random{GREEN}")
                 print(f"Difficulty: {BLUE}Random{GREEN}")
                 print(f"Lives: {BLUE}Random{GREEN}")
-                lines_to_replace += 3
+                lines_to_clear += 3
             elif selection == 2:
-                settings["category"] = "random"
+                game_settings["category"] = "random"
                 print(f"{GREEN}Category reset to default {BLUE}Random{GREEN}.")
             elif selection == 3:
-                settings["difficulty"] = "random"
+                game_settings["difficulty"] = "random"
                 print(f"{GREEN}Difficulty reset to default {BLUE}Random{GREEN}.")
             elif selection == 4:
-                settings["lives"] = 6
+                game_settings["lives"] = 6
                 print(f"{GREEN}Lives reset to default {BLUE}6{GREEN}.")
             else:
                 print(f"{GREEN}Settings not changed.")
 
             input(f"{YELLOW}Press enter to go back to the settings menu.")
-            clear_previous_lines(len(reset_options) + 4 + lines_to_replace)
-        elif selection == 5:
+            clear_previous_lines(len(reset_options) + 4 + lines_to_clear)
+        elif settings_selection == 5:
             print(f"{MAGENTA}Game Options - Show Settings")
             print(f"{GREEN}Current settings:")
-            print(f"Category: {BLUE}{settings['category']}{GREEN}")
-            print(f"Difficulty: {BLUE}{settings['difficulty']}{GREEN}")
-            print(f"Lives: {BLUE}{settings['lives']}{GREEN}")
+            print(f"Category: {BLUE}{game_settings['category']}{GREEN}")
+            print(f"Difficulty: {BLUE}{game_settings['difficulty']}{GREEN}")
+            print(f"Lives: {BLUE}{game_settings['lives']}{GREEN}")
             input(f"{YELLOW}Press enter to go back to the settings menu.")
             clear_previous_lines(6)
-        elif selection == 6:
-            return settings
+        elif settings_selection == 6:
+            return game_settings
 
 
 # tests are in tests/__init__.py
@@ -305,8 +301,8 @@ if __name__ == "__main__":
     lines_to_clear = 0
     previous_state = "main_menu"
     while True:
-        main_menu_selection, invalid_choices = show_main_menu()
-        lines_to_clear += invalid_choices
+        main_menu_selection, invalid_menu_selections = show_main_menu()
+        lines_to_clear += invalid_menu_selections
         lines_to_clear += 2 if previous_state == "hangman_game" else 0
         clear_previous_lines(lines_to_clear)
         lines_to_clear = 0
