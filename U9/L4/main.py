f09ece74f13ce6554839ae14345094ad398ca78f
Marcus Jones <marcusjones8119@gmail.com>
2024-04-03 09:50:51 -0400

    Refactor RLE_decompress function in main.py

--- U9/L4/main.py
+++ U9/L4/main.py
@@ -2,7 +2,7 @@
 # performs Run Length Encoding on the string,
 # and returns a new string representing the compressed message.
 def RLE_compress(message: str) -> None:
-    first_pass = ""
+    compressed = ""
     i = 0
     while i < len(message):
         count = 1
@@ -11,8 +11,25 @@ def RLE_compress(message: str) -> None:
         compressed += message[i] + str(count)
         i += count
 
-    return first_pass
+    return compressed
 
+
+def RLE_decompress(message: str) -> None:
+    decompressed = ""
+    i = 0
+    while i < len(message):
+        char = message[i]
+        i += 1
+        j = i
+        while j < len(message) and message[j].isdigit():
+            j += 1
+        count = int(message[i:j])
+        decompressed += char * count
+        i = j
+    return decompressed
+
+
+print(RLE_decompress("B1R1E11A1K1 1W1E16!1"))
 
 # TEST CODE:
 print(RLE_compress("AABBBAAAABBBBBAAAAAABBBBBBB"))
