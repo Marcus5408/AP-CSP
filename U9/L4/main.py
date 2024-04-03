83d20cba4abc2948f672a81882edf77856761416
Marcus Jones <marcusjones8119@gmail.com>
2024-04-03 09:28:41 -0400

    Implement Run Length Encoding in RLE_compress function

--- U9/L4/main.py
+++ U9/L4/main.py
@@ -2,7 +2,16 @@
 # performs Run Length Encoding on the string,
 # and returns a new string representing the compressed message.
 def RLE_compress(message):
-    return "Finish this function!"
+    first_pass = ""
+    i = 0
+    while i < len(message):
+        count = 1
+        while i + count < len(message) and message[i + count] == message[i]:
+            count += 1
+        first_pass += message[i] + str(count)
+        i += count
+
+    return first_pass
 
 
 # TEST CODE:
@@ -11,7 +20,9 @@ print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFF"))
 print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFD"))
 print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFDD"))
 print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFDDD"))
-print(RLE_compress("ABCDEF"))
+print(
+    RLE_compress("ABCDEF")
+)  # as discussed, this one doesn't actually "compress", but it's a good test case
 print(RLE_compress("FFFFFFFFFFFFFFFFFFF"))
 print(RLE_compress("F"))
 print(RLE_compress("F????"))
